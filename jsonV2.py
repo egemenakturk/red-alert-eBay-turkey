b6654be7d62d9d7c94db81d3413452c2cf4af61a
Egemen Aktürk <eakturk@ebay.com>
2018-07-24 11:01:32 +0300

    Version 0.4

--- jsonV2.py
+++ jsonV2.py
@@ -21,66 +21,62 @@ while True:
     except Exception:
         print()
 
-
-
-
-
-def find_roots(folder_api_urls, job_api_urls, job_failure):
+def isJob(data , x) :
+    if len(data["jobs"][x]) == 3:
+        return 0
+    else:
+        return 1
+    
+def select_roots(folder_api_urls, job_api_urls, job_failure):
     data = requests.get(folder_api_urls[0], headers=headers).json()
     length = len(data["jobs"])
     for x in range(0, length):
-        if len(data["jobs"][x]) == 3:
+        if isJob(data, x) == 0:
             folder_api_urls.append(data["jobs"][x]["url"] + "api/json")
+            print(data["jobs"][x]["url"] + "api/json")
         else:
             if data["jobs"][x]["color"] == "red":
-                job_failure.append(data["jobs"][x]["name"])
+                full_name = data["jobs"][x]["name"]
+                team_name = full_name.split("-")
+                job_failure.append(team_name[0])
     del folder_api_urls[0]
     if len(folder_api_urls) == 0:
         return
     else:
-        find_roots(folder_api_urls, job_api_urls, job_failure)
+        select_roots(folder_api_urls, job_api_urls, job_failure)
 
 
-def job_results(job_api_urls):
+def job_results(job_api_urls, job_failure):
     while len(job_api_urls) != 0:
         data = requests.get(job_api_urls[0], headers=headers).json()
         result_data = requests.get(data["builds"][0]["url"] + "api/json").json()
         if result_data["result"] == "FAILURE":
             full_name = result_data["fullDisplayName"].split(" ")
             team_split = full_name[len(full_name) - 2].split("-")
-            print(team_split[0])
-            if team_split[0] == teams[0]:
-                # hc_07.write(b'6')
-                time.sleep(1.1)
-            if team_split[0] == teams[1]:
-                hc_06.write(b'6')
-                time.sleep(1.1)
-            else:
-                dota.write(b'6')
-                time.sleep(1.1)
+            job_failure.append(team_split)
         del job_api_urls[0]
     return
 
 def send_message(job_failure):
     team_name = job_failure[0]
     print(team_name)
-    team_split = team_name.split("-")
-    if team_split[0] == teams[0]:
+    #print(team_name)
+    if team_name[0] == teams[0]:
         # hc_07.write(b'6')
         time.sleep(1.1)
-    if team_split[0] == teams[1]:
+    if team_name[0] == teams[1]:
         hc_06.write(b'6')
         time.sleep(1.1)
     else:
         dota.write(b'6')
         time.sleep(1.1)
     del job_failure[0]
-    if len(job_failure) ==0 :
+    if len(job_failure) == 0:
         return
     else:
         send_message(job_failure)
 
-find_roots(folder_api_urls, job_api_urls, job_failure)
-job_results(job_api_urls)
+select_roots(folder_api_urls, job_api_urls, job_failure)
+job_results(job_api_urls , job_failure)
 send_message(job_failure)
 print("-----FINISHED-----")
