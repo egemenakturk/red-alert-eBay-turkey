754cadfc57711875f39d86825f1f1b25a41ec9b3
egemenakturk <egemendakturk@gmail.com>
2018-07-27 12:00:17 +0300

    Update gitlab_merge_requests_alerts.py

--- gitlab_merge_requests_alerts.py
+++ gitlab_merge_requests_alerts.py
@@ -9,14 +9,13 @@ import time
 while True:
     try:
         # hc_07 = serial.Serial('/dev/tty.HC-05-SPPDev', 9600)
-        merge_requests_alert = serial.Serial('/dev/tty.HC-06-SPPDev', 9600)
+        merge_requests_alert = serial.Serial('/dev/tty.HC-06-SPPDev-1', 9600)
         # dota_test_fail = serial.Serial('/dev/tty.HC-06-SPPDev-1', 9600)
         break
     except Exception:
         print()
 
 data = requests.get("http://git.app.gittigidiyor.net/api/v4/projects/488/merge_requests?state=opened&private_token=9bQeZz2socxJ2aowMCtJ",).json()
-print(len(data))
 
 if len(data) > 0:
     merge_requests_alert.write(b'6')
